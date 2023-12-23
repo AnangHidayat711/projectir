47e5b52d2cbb829f241e5c0c08cbaab0959c6261
AnangHidayat711 <118416637+AnangHidayat711@users.noreply.github.com>
2023-12-23 18:54:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -100,8 +100,8 @@ def deskripsi():
 
 # if __name__ == '__main__':
 #     app.run(port=3000, debug=True)
-# if __name__ == '__main__':
-#     app.run(debug=True, port=os.getenv("PORT", default=5000))
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=true)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
+
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5000, debug=true)
